15a5931770bae6f08b3172dd5f1e6f1d47d712e1
ahas <dev@ahas.io>
2025-02-18 20:44:54 +0900

    feat: add neovide configs

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -7,18 +7,13 @@ local M = {}
 
 M.base46 = {
 	theme = "catppuccin",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
 }
 
--- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.ui = {
+  statusline = {
+    theme = "minimal",
+    separator_style = "block"
+  }
+}
 
 return M
--- lua/options.lua
+++ lua/options.lua
@@ -3,3 +3,11 @@ require "nvchad.options"
 local o = vim.opt
 
 o.colorcolumn = "108"
+
+-- neovide
+o.guifont = "BlexMono Nerd Font:h14"
+vim.g.neovide_scale_factor = 1.0
+vim.g.neovide_title_background_color = "#252434"
+vim.g.neovide_title_text_color = "#ffffff"
+vim.g.neovide_cursor_smooth_blink = true
+vim.g.neovide_scroll_animation_length = 0.3
