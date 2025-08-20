e9cab5b95ff2b1e023f70d91e9d3865dc653f796
ahas <dev@ahas.io>
2025-08-20 08:27:18 +0500

    chore: add claude-code

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -24,6 +24,7 @@ vim.lsp.config("somesass_ls", {
 local vue_language_server_path = vim.fn.expand "$MASON/packages"
   .. "/vue-language-server"
   .. "/node_modules/@vue/language-server"
+local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
 local vue_plugin = {
   name = "@vue/typescript-plugin",
   location = vue_language_server_path,
@@ -40,39 +41,12 @@ local vtsls_config = {
       },
     },
   },
-  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+  filetypes = tsserver_filetypes,
 }
+local vue_ls_config = {}
 
-local vue_ls_config = {
-  on_init = function(client)
-    client.handlers["tsserver/request"] = function(_, result, context)
-      local clients = vim.lsp.get_clients { bufnr = context.bufnr, name = "vtsls" }
-      if #clients == 0 then
-        vim.notify("Could not find `vtsls` lsp client, `vue_ls` would not work without it.", vim.log.levels.ERROR)
-        return
-      end
-      local ts_client = clients[1]
-
-      local param = unpack(result)
-      local id, command, payload = unpack(param)
-      ts_client:exec_cmd({
-        title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
-        command = "typescript.tsserverRequest",
-        arguments = {
-          command,
-          payload,
-        },
-      }, { bufnr = context.bufnr }, function(_, r)
-        local response_data = { { id, r.body } }
-        ---@diagnostic disable-next-line: param-type-mismatch
-        client:notify("tsserver/response", response_data)
-      end)
-    end
-  end,
-}
-
-vim.lsp.config("vtsls", vtsls_config)
-vim.lsp.config("vue_ls", vue_ls_config)
+vim.lsp.config('vtsls', vtsls_config)
+vim.lsp.config('vue_ls', vue_ls_config)
 
 -- End of Configs
 vim.lsp.enable(servers)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -304,4 +304,33 @@ return {
     build = ":Cord update",
     -- opts = {}
   },
+
+  {
+    "greggh/claude-code.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- Required for git operations
+    },
+    config = function()
+      require("claude-code").setup {
+        keymaps = {
+          toggle = {
+            normal = "<M-,>",
+            terminal = "<M-,>",
+          },
+        },
+        window = {
+          position = "float",
+          float = {
+            width = "90%", -- Take up 90% of the editor width
+            height = "90%", -- Take up 90% of the editor height
+            row = "center", -- Center vertically
+            col = "center", -- Center horizontally
+            relative = "editor",
+            border = "double", -- Use double border style
+          },
+        },
+      }
+    end,
+  },
 }
